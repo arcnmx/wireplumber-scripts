4e4fa12e48f8b2e3facb207c341cd2abb3ef96b9
arcnmx <arcnmx@users.noreply.github.com>
2023-01-31 16:27:02 -0800

    chore(ci): cargo update

--- lock.nix
+++ lock.nix
@@ -1,7 +1,7 @@
 {
   outputHashes = {
     "glib-signal-0.1.0" = "sha256-L2g+t5bSd/bd4xM1tl4cjMFNk39mMCLciCgotjGl9XI=";
-    "wireplumber-0.1.0" = "sha256-Zatv6jFgldiOoYnsgqWInsjRroe84079g9mhtSYriLg=";
-    "wireplumber-sys-0.1.0" = "sha256-Zatv6jFgldiOoYnsgqWInsjRroe84079g9mhtSYriLg=";
+    "wireplumber-0.1.0" = "sha256-qvlWArl/e7WiJ+jHQcAc+vJK1/pDJs6LMb5BrkxSAAY=";
+    "wireplumber-sys-0.1.0" = "sha256-qvlWArl/e7WiJ+jHQcAc+vJK1/pDJs6LMb5BrkxSAAY=";
   };
 }
